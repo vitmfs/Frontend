2c8e5288dc47de628ed737032c83b4bc97bdc36d
Vítor Manuel Ferreira da Silva <vitmfs@hotmail.com>
2025-11-11 19:27:05 +0000

    updated react app

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Whatever from './components/Gallery';
+import { Profile } from './components/Gallery'; //name has to match in both sides
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,8 +28,11 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more2
       </p>
+      <p>Something Else</p>
+      <Whatever />
+      <Profile />
     </>
   )
 }
